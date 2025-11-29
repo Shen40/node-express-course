0c526bbc99ae107e974fb63bb728f3679efdd83e
Shen Chen <shenchen0558@gmail.com>
2025-11-29 15:46:43 -0500

    week2

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -4,13 +4,22 @@ console.log("at start");
 
 writeFile("./temporary/fileB.txt", "This is line 1\n", (err) => {
   console.log("at point 1 (after line 1 write)");
+  
   if (err) return console.log("Error at line 1:", err);
+  
   writeFile("./temporary/fileB.txt", "This is line 2\n", { flag: "a" }, (err) => {
+    
     console.log("at point 2 (after line 2 write)");
+    
     if (err) return console.log("Error at line 2:", err);
+    
     writeFile("./temporary/fileB.txt", "This is line 3\n", { flag: "a" }, (err) => {
+      
+      
       console.log("at point 3 (after line 3 write)");
+      
       if (err) return console.log("Error at line 3:", err);
+      
       console.log("All writes complete!");
     });
   });
